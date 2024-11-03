3d610ff5b4ece92d3a5e12c68d0ceeaccb33c8ab
nickhealthy <alskadmlcraz1@gmail.com>
2024-11-03 16:40:28 +0900

    5. 동적 프록시 기술 - JDK 동적 프록시 - 적용2

---
+++ src/main/java/hello/proxy/config/v1_proxy/v2_dynamicproxy/DynamicProxyFilterConfig.java
@@ -0,0 +1,54 @@
+package hello.proxy.config.v1_proxy.v2_dynamicproxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v1_proxy.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.config.v1_proxy.v2_dynamicproxy.handler.LogTraceFilterHandler;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * JDK 동적 프록시 적용1
+ * 이전에는 프록시 클래스를 직접 하나씩 개발했지만, 이제는 JDK 동적 프록시 기술을 사용해 각각의 컨트롤러, 서비스, 리포지토리에 맞는 동적 프록시를 생성하면 된다.
+ * - no-log 부분은 로그가 남기면 안되기 때문에 특정 조건을 만족할 때만 로그를 남기는 기능을 개발함
+ */
+@Configuration
+public class DynamicProxyFilterConfig {
+
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace trace) {
+        OrderControllerV1 orderControllerV1 = new OrderControllerV1Impl(orderServiceV1(trace));
+
+        OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(orderControllerV1.getClass().getClassLoader(),
+                new Class[]{OrderControllerV1.class},
+                new LogTraceFilterHandler(orderControllerV1, trace, PATTERNS));
+
+        return proxy;
+    }
+
+    private OrderServiceV1 orderServiceV1(LogTrace trace) {
+        OrderServiceV1 orderServiceV1 = new OrderServiceV1Impl(orderRepositoryV1(trace));
+
+        OrderServiceV1 proxy = (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
+                new Class[]{OrderServiceV1.class},
+                new LogTraceFilterHandler(orderServiceV1, trace, PATTERNS));
+
+        return proxy;
+
+    }
+
+    private OrderRepositoryV1 orderRepositoryV1(LogTrace trace) {
+        OrderRepositoryV1 orderRepositoryV1 = new OrderRepositoryV1Impl();
+
+        OrderRepositoryV1 proxy = (OrderRepositoryV1) Proxy.newProxyInstance(OrderRepositoryV1.class.getClassLoader(),
+                new Class[]{OrderRepositoryV1.class},
+                new LogTraceFilterHandler(orderRepositoryV1, trace, PATTERNS));
+
+        return proxy;
+
+    }
+}
---
+++ src/main/java/hello/proxy/config/v1_proxy/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -0,0 +1,53 @@
+package hello.proxy.config.v1_proxy.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.util.PatternMatchUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * JDK 동적 프록시 적용2 - V1 애플리케이션에 JDK 동적 프록시를 적용할 핸드러를 정의
+ * - JDK 동적 프록시는 '인터페이스가 필수'이기 때문에 V1 애플리케이션만 적용 가능
+ * - no-log 부분은 로그가 남기면 안되기 때문에 특정 조건을 만족할 때만 로그를 남기는 기능을 개발함
+ */
+public class LogTraceFilterHandler implements InvocationHandler {
+
+    private final Object target;    // 프록시가 호출할 실제 대상
+    private final LogTrace trace;
+    private final String[] patterns;
+
+    public LogTraceFilterHandler(Object target, LogTrace trace, String[] patterns) {
+        this.target = target;
+        this.trace = trace;
+        this.patterns = patterns;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        // 메서드 이름 필터
+        String methodName = method.getName();
+        if (!PatternMatchUtils.simpleMatch(patterns, methodName)) {
+            // 매칭이 안된 메서드들은 실제 서비스 로직만 수행하고, 종료(프록시의 부가 기능 사용 X)
+            return method.invoke(target, args);
+        }
+
+        TraceStatus status = null;
+
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = trace.begin(message);
+
+            // 로직 호출
+            Object result = method.invoke(target, args);
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
