170b0306666ca45bb1ed4e60e716e3c91869e1ac
nickhealthy <alskadmlcraz1@gmail.com>
2024-11-06 23:12:47 +0900

    6. 스프링이 지원하는 프록시 - 프록시 팩토리 - 적용2

--- src/main/java/hello/proxy/config/v3_proxyfactory/ProxyFactoryConfigV2.java
+++ src/main/java/hello/proxy/config/v3_proxyfactory/ProxyFactoryConfigV2.java
@@ -1,6 +1,9 @@
 package hello.proxy.config.v3_proxyfactory;
 
 import hello.proxy.app.v1.*;
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
 import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
@@ -12,45 +15,45 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 프록시 팩토리 적용1 - 인터페이스가 있는 구현체(JDK 동적 프록시)
+ * 프록시 팩토리 적용2 - 인터페이스가 없는 구현체(CGLIB)
  * 이전에는 JDK 동적 프록시, CGLIB 프록시 구현 방식에 따라 개별적으로 프록시 클래스를 지정해서 설정해야했지만,
  * 이제는 프록시 팩토리를 통해 구현 방식에 상관없이 동일한 방식으로 프록시를 적용할 수 있다.
  */
 @Slf4j
 @Configuration
-public class ProxyFactoryConfigV1 {
+public class ProxyFactoryConfigV2 {
 
     @Bean
-    public OrderControllerV1 orderControllerV1(LogTrace trace) {
-        OrderControllerV1 orderController = new OrderControllerV1Impl(orderServiceV1(trace));
+    public OrderControllerV2 orderControllerV2(LogTrace trace) {
+        OrderControllerV2 orderController = new OrderControllerV2(orderServiceV2(trace));
         ProxyFactory factory = new ProxyFactory(orderController);
         factory.addAdvisor(getAdvisor(trace));
 
-        OrderControllerV1 proxy = (OrderControllerV1) factory.getProxy();
+        OrderControllerV2 proxy = (OrderControllerV2) factory.getProxy();
         log.info("ProxyFactory proxy={}, target={}", proxy.getClass(),
                 orderController.getClass());
         return proxy;
     }
 
     @Bean
-    public OrderServiceV1 orderServiceV1(LogTrace trace) {
-        OrderServiceV1 orderService = new OrderServiceV1Impl(orderRepositoryV1(trace));
+    public OrderServiceV2 orderServiceV2(LogTrace trace) {
+        OrderServiceV2 orderService = new OrderServiceV2(orderRepositoryV2(trace));
         ProxyFactory factory = new ProxyFactory(orderService);
         factory.addAdvisor(getAdvisor(trace));
 
-        OrderServiceV1 proxy = (OrderServiceV1) factory.getProxy();
+        OrderServiceV2 proxy = (OrderServiceV2) factory.getProxy();
         log.info("ProxyFactory proxy={}, target={}", proxy.getClass(),
                 orderService.getClass());
         return proxy;
     }
 
     @Bean
-    public OrderRepositoryV1 orderRepositoryV1(LogTrace trace) {
-        OrderRepositoryV1 orderRepository = new OrderRepositoryV1Impl();
+    public OrderRepositoryV2 orderRepositoryV2(LogTrace trace) {
+        OrderRepositoryV2 orderRepository = new OrderRepositoryV2();
         ProxyFactory factory = new ProxyFactory(orderRepository);
         factory.addAdvisor(getAdvisor(trace));
 
-        OrderRepositoryV1 proxy = (OrderRepositoryV1) factory.getProxy();
+        OrderRepositoryV2 proxy = (OrderRepositoryV2) factory.getProxy();
         log.info("ProxyFactory proxy={}, target={}", proxy.getClass(),
                 orderRepository.getClass());
         return proxy;
